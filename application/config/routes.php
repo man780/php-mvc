6561c3f24433a18cc71c046931f99f5dff8b42e5
Murod <blogermurod@gmail.com>
2021-02-13 14:02:29 +0500

    Account MVC added

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,4 +7,13 @@ return [
 		'action' => 'index',
 	],
 	
+	'account/login' => [
+		'controller' => 'account',
+		'action' => 'login',
+	],
+
+	'account/register' => [
+		'controller' => 'account',
+		'action' => 'register',
+	],
 ];
